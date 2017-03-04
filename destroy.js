00aba0e6c3ddf9066d96c67cf27184ff2543eabd
Blaine Schmeisser <blainesch@gmail.com>
2017-03-03 16:24:49 -0800

    Fix ci files.

--- destroy.js
+++ destroy.js
@@ -3,6 +3,8 @@ const { exec } = require('child_process')
 module.exports = () => {
   return (pid, env = {}) => {
     return new Promise((resolve, reject) => {
+      // kill -9
+      // Stop-Process
       exec(`kill -9 ${pid}`, (error, stdout) => {
         error ? reject(error) : resolve()
       })
